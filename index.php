a3b7883244ac50b19f17e0bae15282e879308e62
sinisaglavas <102516546+sinisaglavas@users.noreply.github.com>
2022-08-06 11:01:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+
+$animal_speed =
+    [
+        ['animal'=>'rabbit','speed'=>22],
+        ['animal'=>'elephant','speed'=>52],
+        ['animal'=>'cheetah','speed'=>3],//gepard
+        ['animal'=>'antelope','speed'=>7],
+        ['animal'=>'fox','speed'=>16],
+    ];
+
+
+function animal_catch(){
+    global $animal_speed;
+    $random_key = array_rand($animal_speed,1);
+    //for ($i=0;$i<count($animal_speed);$i++)
+   //{
+        if ($animal_speed[$random_key]['speed']>20 || $animal_speed[$random_key]['speed']>40){
+            echo $animal_speed[$random_key]['animal'];
+        }elseif ($animal_speed[$random_key]['speed']>10){
+            echo $animal_speed[$random_key]['animal'];
+        }elseif ($animal_speed[$random_key]['speed']>1 && $animal_speed[$random_key]['speed']<10){
+            $random_key1 = array_rand($animal_speed,1);
+                echo $animal_speed[$random_key1]['animal'];
+        }
+   // }
